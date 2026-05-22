2fcec4e4a834fecd010663bc786450e6f312bba7
m.nushath <m.nushath@pegb.tech>
2026-05-22 19:37:33 +0400

    Initial commit

---
+++ src/main/java/com/nushadlabs/payment_reconcilation/PaymentReconcilationApplication.java
@@ -0,0 +1,13 @@
+package com.nushadlabs.payment_reconcilation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentReconcilationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentReconcilationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nushadlabs/payment_reconcilation/PaymentReconcilationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nushadlabs.payment_reconcilation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentReconcilationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
